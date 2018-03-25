da9b968e03ad5e523ce6c0086a025dfda4c070c8
dradford <damienradford@gmail.com>
2018-03-24 19:02:39 -0700

    improved the js

--- scraper/program-scraper.js
+++ scraper/program-scraper.js
@@ -1,19 +1,40 @@
 const rp = require('request-promise');
 const cheerio = require('cheerio');
 
-const options = {
-  uri: `https://bugcrowd.com/programs`,
-  transform: function (body) {
-    return cheerio.load(body);
-  }
-};
+var bugcrowd = {
+  getPrograms: function(output) {
+    return rp({
+      uri: `https://bugcrowd.com/programs`,
+      transform: function (body) {
+        return cheerio.load(body);
+      }
+    }).then(($) => {
+      output['programs'] = [];
+      $('.bc-panel__title').each(function(i, elem) {
+        output['programs'].push($(this).text().trim())
+      });
+      return output;
+    })
+  },
+  getVrt: function(output) {
+    return rp({
+      uri: `https://raw.githubusercontent.com/bugcrowd/vulnerability-rating-taxonomy/master/vulnerability-rating-taxonomy.json`,
+      transform: function (body) {
+        return JSON.parse(body);
+      }
+    }).then(($) => {
+      console.log($);
+      return output;
+    })
+  },
+}
 
-rp(options)
-  .then(($) => {
-    $('.bc-panel__title').each(function(i, elem) {
-      console.log($(this).text());
-    });
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+function main() {
+  var output = {};
+  return bugcrowd.getPrograms(output)
+                 .then(bugcrowd.getVrt(output));
+}
+
+main().then(function(result) {
+  console.log(result);
+});
